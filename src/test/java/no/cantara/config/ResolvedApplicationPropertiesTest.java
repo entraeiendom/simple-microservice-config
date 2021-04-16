10250663043441e56af0d992b58829589ac9b8df
Tine Kleivane <kleivane@gmail.com>
2021-04-16 14:58:41 +0200

    Testing ordering and override

--- src/test/java/no/cantara/config/ResolvedApplicationPropertiesTest.java
+++ src/test/java/no/cantara/config/ResolvedApplicationPropertiesTest.java
@@ -36,6 +36,60 @@ public class ResolvedApplicationPropertiesTest {
         assertThat(applicationProperties.getProperties()).isEqualTo(properties);
     }
 
+    @Test
+    public void orderMatters_withPropertiesOverridesAll() {
+        final Properties properties = getProperties("first.key", "first.value");
+        properties.setProperty("second.key", "second.value");
+        final ResolvedApplicationProperties applicationProperties = ResolvedApplicationProperties.Builder
+                .builder()
+                .withProperty("first.property", "first.value")
+                .withProperties(properties)
+                .build();
+        assertThat(applicationProperties.get("first.key")).isEqualTo("first.value");
+        assertThat(applicationProperties.get("second.key")).isEqualTo("second.value");
+        assertThat(applicationProperties.get("first.property")).isNull();
+
+        assertThat(applicationProperties.getProperties()).isEqualTo(properties);
+    }
+
+    @Test
+    public void orderMatters_withPropertyAdds() {
+        final Properties properties = getProperties("first.key", "first.value");
+        properties.setProperty("second.key", "second.value");
+        final ResolvedApplicationProperties applicationProperties = ResolvedApplicationProperties.Builder
+                .builder()
+                .withProperties(properties)
+                .withProperty("last.property", "last.value")
+                .build();
+        assertThat(applicationProperties.get("first.key")).isEqualTo("first.value");
+        assertThat(applicationProperties.get("second.key")).isEqualTo("second.value");
+        assertThat(applicationProperties.get("last.property")).isEqualTo("last.value");
+
+        final Properties resolved = new Properties(properties);
+        resolved.setProperty("first.key", "first.value");
+        resolved.setProperty("second.key", "second.value");
+        resolved.setProperty("last.property", "last.value");
+        assertThat(applicationProperties.getProperties()).isEqualTo(resolved);
+    }
+
+    @Test
+    public void orderMatters_withPropertyCanOverride() {
+        final Properties properties = getProperties("first.key", "first.value");
+        properties.setProperty("second.key", "second.value");
+        final ResolvedApplicationProperties applicationProperties = ResolvedApplicationProperties.Builder
+                .builder()
+                .withProperties(properties)
+                .withProperty("second.key", "different.value")
+                .build();
+        assertThat(applicationProperties.get("first.key")).isEqualTo("first.value");
+        assertThat(applicationProperties.get("second.key")).isEqualTo("different.value");
+
+        final Properties resolved = new Properties(properties);
+        resolved.setProperty("first.key", "first.value");
+        resolved.setProperty("second.key", "different.value");
+        assertThat(applicationProperties.getProperties()).isEqualTo(resolved);
+    }
+
     private Properties getProperties(String key, String value) {
         final Properties properties = new Properties();
         properties.setProperty(key, value);
