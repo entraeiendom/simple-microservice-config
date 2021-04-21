a66b3aa8f35b6fcfb85d467842719b81d3073cc7
Tine Kleivane <kleivane@gmail.com>
2021-04-21 10:06:10 +0200

    Add reset-behavior applicable when running mvn test

--- src/test/java/no/cantara/config/ApplicationPropertiesSingeltonTest.java
+++ src/test/java/no/cantara/config/ApplicationPropertiesSingeltonTest.java
@@ -1,10 +1,22 @@
 package no.cantara.config;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApplicationPropertiesSingeltonTest {
+
+
+    @Before
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field instance = ApplicationProperties.class.getDeclaredField("singleton");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
 
 
     @Test(expected = RuntimeException.class)
