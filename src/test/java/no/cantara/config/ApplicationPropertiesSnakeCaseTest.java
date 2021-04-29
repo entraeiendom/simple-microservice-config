e80b6a7148b9764776f9b2e352e0000e778c0851
baardl <bard.lind@gmail.com>
2021-04-29 13:27:26 +0200

    #1 Support for snake_case environment variables

---
+++ src/test/java/no/cantara/config/ApplicationPropertiesSnakeCaseTest.java
@@ -0,0 +1,50 @@
+package no.cantara.config;
+
+import no.cantara.config.testsupport.ApplicationPropertiesTestHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static no.cantara.config.ApplicationPropertiesSnakeCaseTest.ApiExpectedApplicationProperties.BASE_URL;
+import static no.cantara.config.ApplicationPropertiesSnakeCaseTest.ApiExpectedApplicationProperties.SNAKE_CASE_TEST;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class ApplicationPropertiesSnakeCaseTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty(BASE_URL,"http-value");
+        ApplicationPropertiesTestHelper.resetApplicationProperties();
+//        ApplicationProperties.builderWithDefaults().build();
+        ApplicationProperties.builderWithDefaults()
+                .property("JAVA_HOME", "/usr/lib/jvm/java-14-openjdk-amd64")
+                .enableSystemProperties()
+                .enableEnvironmentVariables("")
+                .expectedProperties(ApiExpectedApplicationProperties.class)
+                .buildAndSetStaticSingleton();
+        
+    }
+
+    @Test
+    public void snakeCaseEnvironment() {
+        ApplicationProperties applicationProperties = ApplicationProperties.getInstance();
+        assertEquals("http-value", applicationProperties.get("BASE_URL"));
+        assertEquals("http-value",applicationProperties.get(BASE_URL));
+        assertEquals("from application properties", applicationProperties.get("SNAKE_CASE_TEST"));
+        assertEquals("from application properties", applicationProperties.get(SNAKE_CASE_TEST));
+    }
+
+    public class ApiExpectedApplicationProperties {
+
+        public static final String BASE_URL = "base.url";
+        public static final String SNAKE_CASE_TEST = "SNAKE_CASE_TEST";
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ApplicationPropertiesTestHelper.resetApplicationProperties();
+    }
+}
