a3e3dfa9f4a6e175d231e08477e441584bc3899f
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-05-04 09:27:02 +0200

    All properties visible in sub-map test

--- src/test/java/no/cantara/config/SubMapTest.java
+++ src/test/java/no/cantara/config/SubMapTest.java
@@ -11,7 +11,12 @@ public class SubMapTest {
 
     @Test
     public void shouldGetPropertiesWithSingleProperty() {
-        ApplicationProperties instance = ApplicationProperties.builderWithDefaults()
+        ApplicationProperties instance = ApplicationProperties.builder()
+                .values()
+                .put("level1.l2.a", "A")
+                .put("level1.l2.l3.b", "B")
+                .put("level1.l2.c", "C")
+                .end()
                 .build();
 
         Map<String, String> l2 = instance.subMap("level1.l2.");
