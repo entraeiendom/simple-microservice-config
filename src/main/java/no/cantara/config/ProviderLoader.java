5eb0dde319c1f2f248c0cc312451faecb810c170
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-06-09 15:59:16 +0200

    rename internal variable to factory

--- src/main/java/no/cantara/config/ProviderLoader.java
+++ src/main/java/no/cantara/config/ProviderLoader.java
@@ -6,14 +6,14 @@ public class ProviderLoader {
 
     public static <R, F extends ProviderFactory<R>> F factoryOf(String providerIdOrClassname, Class<F> factoryClazz) {
         ServiceLoader<F> loader = ServiceLoader.load(factoryClazz);
-        for (F providerInitializer : loader) {
-            String providerId = providerInitializer.alias();
-            Class<?> providerClass = providerInitializer.providerClass();
+        for (F factory : loader) {
+            String providerId = factory.alias();
+            Class<?> providerClass = factory.providerClass();
             if (providerIdOrClassname.equals(providerId)
                     || providerIdOrClassname.equals(providerClass.getName())
                     || providerIdOrClassname.equals(providerClass.getSimpleName())
             ) {
-                return providerInitializer;
+                return factory;
             }
         }
         throw new RuntimeException("No " + factoryClazz.getSimpleName() + " provider found for providerIdOrClassname: " + providerIdOrClassname);
@@ -21,14 +21,14 @@ public class ProviderLoader {
 
     public static <R, T extends ProviderFactory<R>> R configure(ApplicationProperties applicationProperties, String providerIdOrClassname, Class<T> clazz) {
         ServiceLoader<T> loader = ServiceLoader.load(clazz);
-        for (T providerInitializer : loader) {
-            String providerId = providerInitializer.alias();
-            Class<?> providerClass = providerInitializer.providerClass();
+        for (T factory : loader) {
+            String providerId = factory.alias();
+            Class<?> providerClass = factory.providerClass();
             if (providerIdOrClassname.equals(providerId)
                     || providerIdOrClassname.equals(providerClass.getName())
                     || providerIdOrClassname.equals(providerClass.getSimpleName())
             ) {
-                return providerInitializer.create(applicationProperties);
+                return factory.create(applicationProperties);
             }
         }
         throw new RuntimeException("No " + clazz.getSimpleName() + " provider found for providerIdOrClassname: " + providerIdOrClassname);
